d231a93c407922a80617861bd8e8b297633db74f
Ed <eduramq@gmail.com>
2023-01-24 22:02:48 -0700

    Displayed dynamic info for singular and every post

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/admin','AdminsController@index')->name('admin.index');
-Route::get('/post', 'PostController@show')->name('post');
+Route::get('/post/{post}', 'PostController@show')->name('post');
