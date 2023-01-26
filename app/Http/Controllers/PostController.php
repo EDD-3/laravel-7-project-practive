9d6d704ab360220a5b9ee909bb08629a0b01a41f
Ed <eduramq@gmail.com>
2023-01-25 21:25:14 -0700

    Added post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,4 +12,14 @@ class PostController extends Controller
         
         return view('blog-post', ['post' => $post]);
     }
+
+    public function create () {
+        return view('admin.posts.create');
+    }
+
+    public function store () {
+        auth()->user();
+        dd(request()->all());
+
+    }
 }
